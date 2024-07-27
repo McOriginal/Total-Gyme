b3ec6659a17d90a762458c0a33f3339f76ec368a
McOriginal <originalmc223gmail.com>
2024-07-27 12:24:11 +0000

    finalisation

--- script.js
+++ script.js
@@ -49,15 +49,19 @@ menu.addEventListener('click', ()=>{
     });
 
 
-// sections.forEach(sec => {
-//     const top = window.scrollY;
-//     const offset = sec.offsetTop - 150;
-//     const height = sec.offsetHeight;
-//     let id = sec.getAttribute('id');
 
-// console.log(top);
-// console.log(offset);
-// console.log(height);
-// console.log(id);
 
-// });
+    const dialogue = document.querySelector('.dialogue');
+    const showDialog = document.querySelectorAll(".showDialogue");
+    const closeDialog = document.querySelector(".closeDialogue");
+
+
+showDialog.forEach(e =>{
+    e.addEventListener('click', ()=>{
+        dialogue.style.display = 'block';
+    });
+})
+
+closeDialog.addEventListener('click', ()=>{
+    dialogue.style.display = 'none';
+});
